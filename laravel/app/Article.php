2276f438a57a7489710a47c1b5bf54b7f722eef7
Ilia Benson <iliabenson@yahoo.com>
2015-06-17 12:08:22 -0700

    finished video 14, all code updated and tested up to that point. all is working

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -10,10 +10,12 @@ class Article extends Model
 	protected $fillable = [
 		'title',
 		'body',
-		'published_at'
+		'published_at',
+		'user_id' // temporary
 	];
 
-	protected $dates = ['published_at'];
+	protected $dates = ['published_at']; // tells eloquent that published_at should be treated as carbon instance. NOT SURE HOW THIS WORKS.
+	// is dates a special variable that is recognized by eloquent/mysql/laravel?
 
 	public function scopePublished($query){
 		$query->where('published_at', '<=', Carbon::now());
@@ -23,7 +25,11 @@ class Article extends Model
 		$query->where('published_at', '>', Carbon::now());
 	}
 
-	public function setPublishedAtAttribute($date){
+	public function setPublishedAtAttribute($date){ // this is a mutator. it takes a an element of one type and mutates it to another type.
 		$this->attributes['published_at'] = Carbon::parse($date);
+	}
+
+	public function user(){
+		return $this->belongsTo('App\User');
 	}
 }
--- laravel/database/migrations/2015_06_11_183020_create_articles_table.php
+++ laravel/database/migrations/2015_06_11_183020_create_articles_table.php
@@ -5,29 +5,35 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateArticlesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('articles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
-            $table->timestamp('published_at');
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->string('title');
+			$table->text('body');
+			$table->timestamps();
+			$table->timestamp('published_at');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('articles');
-    }
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
 }
