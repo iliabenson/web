7403f706c7d53f17648249c35b9f5c8f92c456a6
Ilia Benson <iliabenson@yahoo.com>
2015-06-22 15:37:36 -0700

    finished tutorial number 9. all is working

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -16,6 +16,9 @@ Route::get('/', 'WelcomeController@index');
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
 
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
+
 // Route::get('home', 'HomeController@index');
 
 // Route::controllers([
