48dd7bc0de5a0cc866555e0f4e0740c8944e8dda
Ilia Benson <iliabenson@yahoo.com>
2015-06-24 13:54:18 -0700

    finished tutorial number 16. all is working, it was a review

--- laravel/app/Http/Controllers/ArticlesController.php
+++ laravel/app/Http/Controllers/ArticlesController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth; // needed for Auth in store. look up facade
 class ArticlesController extends Controller {
 
 	public function __construct(){
-		// look up middleware, in a nutshell it is basically extra functionality that is processed in between client request and server response.
+		// look up middleware, in a nutshell it is basically extra functionality that is processed in between client request and server response, like check if your logged in.
 		$this->middleware('auth', ['only' => 'create']); // can be attached at the route level directly or with an anonymous function. 
 		// if you do create your own middleware (via php artisan), make sure to enable it in Requests->Kernel, can make global (each request gets checked) or route specific (creates keyword and is assigned to route, like auth above).
 	}
